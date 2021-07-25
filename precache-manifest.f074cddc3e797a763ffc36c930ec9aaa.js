0720fdf772c95458fbac6820b4848d70b269bfef
Jesus Vazquez <jesusvazquez@jesuss-mac-mini.lan>
2021-07-24 20:06:30 -0600

    Updates

---
+++ precache-manifest.f074cddc3e797a763ffc36c930ec9aaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ce63fac6b1cedc57168dcd740cfdbe",
+    "url": "/ventas/index.html"
+  },
+  {
+    "revision": "83537ff4bc0eeb026d50",
+    "url": "/ventas/static/css/main.4ebcd86c.chunk.css"
+  },
+  {
+    "revision": "429d4a8e75b7d6ab690e",
+    "url": "/ventas/static/js/2.a8e3b57b.chunk.js"
+  },
+  {
+    "revision": "13438eee4e777095ed4206c277e09443",
+    "url": "/ventas/static/js/2.a8e3b57b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83537ff4bc0eeb026d50",
+    "url": "/ventas/static/js/main.19bd60cd.chunk.js"
+  },
+  {
+    "revision": "93fde1510c568a8c72e2",
+    "url": "/ventas/static/js/runtime-main.36401825.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/ventas/static/media/shopping-bag.30f07071.svg"
+  }
+]);
